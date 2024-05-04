6c6c41bd308de44b23e841bd0324a874ca3e032a
ashutosh <vkrypto@gmail.com>
2024-05-05 03:53:13 +0530

    adding s3 upload api

--- app/main.py
+++ app/main.py
@@ -1,20 +1,32 @@
-from fastapi import FastAPI
 import uvicorn
+from typing import List
+from fastapi import FastAPI, File, UploadFile, Form
+import asyncio
 
 from src.routers.linked_in import router as linked_in_router
 from src.middleware import URLLoggerMiddleware
+from src.utils.storage import FileManager
+
 
 app = FastAPI()
 
 app.add_middleware(URLLoggerMiddleware)
+
 app.include_router(linked_in_router, prefix="/linkedin")
+
+
+@app.post("/upload/")
+async def upload_files(folder_name: str = Form(...), files: List[UploadFile] = File(...)):
+    upload_tasks = [FileManager.upload_file(file, folder_name) for file in files]
+    uploaded_files = await asyncio.gather(*upload_tasks)
+    return {"uploaded_files": uploaded_files}
 
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
-        port=8000,
+        port=8001,
         reload=True,
         reload_dirs="./src"
     )
---
+++ app/src/utils/storage.py
@@ -0,0 +1,32 @@
+import boto3
+from typing import List
+import uuid
+from decouple import config
+
+
+class FileManager:
+    
+    AWS_REGION_NAME=config("AWS_REGION_NAME", None)
+    __s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=config("AWS_ACCESS_KEY_ID", None),
+        aws_secret_access_key=config("AWS_SECRET_ACCESS_KEY", None),
+        region_name = AWS_REGION_NAME
+    )
+    BUCKET_NAME = config("BUCKET_NAME", None)
+    s3_url_prefix = f"https://s3.{AWS_REGION_NAME}.amazonaws.com/{BUCKET_NAME}"
+    
+    @classmethod
+    async def upload_file(cls, file, folder_name) -> List[str]:
+        hash = str(uuid.uuid4())[:10]
+        file_name = f"{folder_name}/{hash}_{file.filename[-10:]}"
+        # UploadFile(filename='25729859.jpeg', 
+        #   size=19079, 
+        #   headers=Headers(
+            # {
+            #  'content-disposition': 'form-data; name="files"; filename="25729859.jpeg"',
+            #  'content-type': 'image/jpeg'}
+            # )
+        # )
+        cls.__s3_client.upload_fileobj(file.file, cls.BUCKET_NAME, file_name)
+        return f"{cls.s3_url_prefix}/{file_name}"
