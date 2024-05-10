0abaf0d3e64625714233cfcd2e5a0793c79302a8
ashutosh <vkrypto@gmail.com>
2024-05-10 13:05:30 +0530

    auto syncing..

---
+++ readme_generator/update_readme.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+working__dir = os.path.dirname(str(__file__))
+
+def main():
+    # get root-template path 
+    render_env = Environment(loader=FileSystemLoader(working__dir))
+    template = render_env.get_template('index.md')
+    # get context data
+    with open(os.path.join(working__dir, "data.yml"), 'r') as file:
+        context = yaml.safe_load(file)
+    # render template
+    output = template.render(context)
+    if not output:
+        raise Exception("Could not render template")
+    # write into final readme.md
+    with open(os.path.join("README.md"), 'w') as file:
+        file.write(output)
+
+
+if __name__ == "__main__":
+    main()
+
